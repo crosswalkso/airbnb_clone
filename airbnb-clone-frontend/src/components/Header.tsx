9e09b298fd77245518c18632776c348658c99fb1
crosswalkso <crosswalk0628@gmail.com>
2022-11-09 16:30:24 +0900

    Sec18.3 Dark mode -ifelse

--- airbnb-clone-frontend/src/components/Header.tsx
+++ airbnb-clone-frontend/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { FaAirbnb, FaMoon } from "react-icons/fa";
-import { Box, Button, HStack, IconButton, useDisclosure } from "@chakra-ui/react";
+import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
+import { Box, Button, HStack, IconButton, LightMode, useColorMode, useDisclosure } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
@@ -7,6 +7,7 @@ import SignUpModal from "./SignUpModal";
 export default function Header() {
   const { isOpen: isLoginOpen, onClose: onLoginClose, onOpen: onLoginOpen } = useDisclosure();
   const { isOpen: isSignUpOpen, onClose: onSignUpClose, onOpen: onSignUpOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack justifyContent={"space-between"} py={5} px={10} borderBottomWidth={1}>
       <Box color="red.500">
@@ -15,11 +16,18 @@ export default function Header() {
         </Link>
       </Box>
       <HStack spacing={2}>
-        <IconButton variant={"ghost"} aria-label="Toggle dark mode" icon={<FaMoon />} />
+        <IconButton
+          onClick={toggleColorMode}
+          variant={"ghost"}
+          aria-label="Toggle dark mode"
+          icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
+        />
         <Button onClick={onLoginOpen}>Log in</Button>
-        <Button onClick={onSignUpOpen} colorScheme={"red"}>
-          Sign up
-        </Button>
+        <LightMode>
+          <Button onClick={onSignUpOpen} colorScheme={"red"}>
+            Sign up
+          </Button>
+        </LightMode>
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
       <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
