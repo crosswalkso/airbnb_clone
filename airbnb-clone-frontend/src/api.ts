517652a87a00950665e9b6121c580955a59be9cb
crosswalkso <crosswalk0628@gmail.com>
2022-11-14 15:59:02 +0900

    Sec20.4 CSRF

--- airbnb-clone-frontend/src/api.ts
+++ airbnb-clone-frontend/src/api.ts
@@ -1,3 +1,5 @@
+import Cookie from "js-cookie";
+
 import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -20,4 +22,11 @@ export const getRoomReviews = ({ queryKey }: QueryFunctionContext) => {
 
 export const getMe = () => instance.get(`users/me`).then((response) => response.data);
 
-export const logOut = () => instance.post(`users/log-out`).then((response) => response.data);
+export const logOut = () =>
+  instance
+    .post(`users/log-out`, null, {
+      headers: {
+        "X-CSRFToken": Cookie.get("csrftoken") || "",
+      },
+    })
+    .then((response) => response.data);
