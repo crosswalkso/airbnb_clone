1d351220ac3c377fb7132ca2f692e6c1a78aae6d
crosswalkso <crosswalk0628@gmail.com>
2022-11-24 14:51:09 +0900

    Sec21.5 Upload Form

--- airbnb-clone-frontend/src/components/Room.tsx
+++ airbnb-clone-frontend/src/components/Room.tsx
@@ -1,22 +1,38 @@
 import { Box, Button, Grid, HStack, Image, Text, useColorModeValue, VStack } from "@chakra-ui/react";
-import { FaRegHeart, FaStar } from "react-icons/fa";
+import React from "react";
+import { FaCamera, FaRegHeart, FaStar } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 
 interface IRoomProps {
   imageUrl: string;
   name: string;
   rating: number;
   price: number;
+  pk: number;
+  isOwner: boolean;
 }
 
-export default function Room({ imageUrl, name, rating, price }: IRoomProps) {
+export default function Room({ imageUrl, name, rating, price, isOwner }: IRoomProps) {
   const gray = useColorModeValue("gray.600", "gray.300");
+  const navigate = useNavigate();
+  const onCameraClick = (event: React.SyntheticEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    navigate(`/rooms/${pk}/photos`);
+  };
   return (
     <Link to={`/rooms/${pk}`}>
       <VStack alignItems={"flex-start"}>
         <Box position="relative" overflow={"hidden"} mb={3} rounded="3xl">
           <Image h="100%" src={imageUrl} />
-          <Button variant={"unstyled"} position="absolute" top={0} right={0} color="white">
-            <FaRegHeart size="20px" />
+          <Button
+            variant={"unstyled"}
+            position="absolute"
+            top={0}
+            right={0}
+            onClick={onCameraClick}
+            color="white"
+          >
+            {isOwner ? <FaCamera size="20px" /> : <FaRegHeart size="20px" />}
           </Button>
         </Box>
         <Box>
